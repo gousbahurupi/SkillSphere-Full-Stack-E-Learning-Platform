b1c7758a6d1ce46e03a634b36f38fa28340343f0
gous <gousbahurupi5@gmail.com>
2026-01-18 15:17:53 +0530

    frontend redy to deploy

--- frontend/edusphere-client/src/context/AuthContext.jsx
+++ frontend/edusphere-client/src/context/AuthContext.jsx
@@ -36,7 +36,7 @@ export const AuthProvider = ({ children }) => {
 
   // ✅ REGISTER  (THIS MUST EXIST)
   const register = async (formData) => {
-    const res = await fetch("http://localhost:5000/api/auth/register", {
+    const res = await fetch("https://skill-sphere-vioy.onrender.com/api/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
